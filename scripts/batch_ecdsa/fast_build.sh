d6765ba5d5aa47952f608cffe33f4879345385d5
Uma Roy <uma@Umas-MBP-3.localdomain>
2022-06-15 16:48:50 -0700

    Changed where fast_build.sh writes witness to

--- scripts/batch_ecdsa/fast_build.sh
+++ scripts/batch_ecdsa/fast_build.sh
@@ -18,19 +18,17 @@ echo "DONE ($((end-start))s)"
 echo "****Running make to make witness generation binary****"
 start=`date +%s`
 make -C "$BUILD_DIR"/"$CIRCUIT_NAME"_cpp
-cp "$BUILD_DIR"/"$CIRCUIT_NAME"_cpp/"$CIRCUIT_NAME" "$BUILD_DIR"
-cp "$BUILD_DIR"/"$CIRCUIT_NAME"_cpp/"$CIRCUIT_NAME".dat "$BUILD_DIR"
 end=`date +%s`
 echo "DONE ($((end-start))s)"
 
 echo "****Executing witness generation****"
 start=`date +%s`
-./"$BUILD_DIR"/"$CIRCUIT_NAME" input_batch_ecdsa_rprime.json "$BUILD_DIR"/witness.wtns
+./"$BUILD_DIR"/"$CIRCUIT_NAME"_cpp/"$CIRCUIT_NAME" input_batch_ecdsa_rprime.json "$BUILD_DIR"/"$CIRCUIT_NAME"_cpp/witness.wtns
 end=`date +%s`
 echo "DONE ($((end-start))s)"
 
 echo "****Converting witness to json****"
 start=`date +%s`
-npx snarkjs wej "$BUILD_DIR"/witness.wtns "$BUILD_DIR"/witness.json
+npx snarkjs wej "$BUILD_DIR"/"$CIRCUIT_NAME"_cpp/witness.wtns "$BUILD_DIR"/"$CIRCUIT_NAME"_cpp/witness.json
 end=`date +%s`
 echo "DONE ($((end-start))s)"
