81c231c2c9c423808b3aa95bb1a6506fc64a2eb8
John Guibas <jtguibas@ip-172-31-4-100.us-west-1.compute.internal>
2022-07-20 22:26:57 +0000

    delete unused test files

--- scripts/batch_ecdsa/benchmark.sh
+++ scripts/batch_ecdsa/benchmark.sh
@@ -1,6 +1,6 @@
 mkdir -p logs
-yarn build:c 2 | tee logs/2.log && \
-yarn build:c 4 | tee logs/4.log && \
-yarn build:c 8 | tee logs/8.log && \
-yarn build:c 16 | tee logs/16.log && \
-yarn build:c 32 | tee logs/32.log
+# yarn build:c 2 | tee logs/batch2.log && \
+# yarn build:c 4 | tee logs/batch4.log && \
+# yarn build:c 8 | tee logs/batch8.log && \
+yarn build:c 16 | tee logs/batch16.log && \
+yarn build:c 32 | tee logs/batch32.log
--- test/circuits/test_batch_ecdsa_verify_128.circom
+++
@@ -1,5 +0,0 @@
-pragma circom 2.0.5;
-
-include "../../circuits/batch_ecdsa.circom";
-
-component main {public [r, rprime, s, msghash, pubkey]} = BatchECDSAVerifyNoPubkeyCheck(64, 4, 128);
